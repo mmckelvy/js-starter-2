18f02f2fea7a6709688c8a59694d418d5afdedbf
Mark McKelvy <mmckelvy@gmail.com>
2017-02-12 10:59:03 -0800

    set up react scaffolding

--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello world')
+import 'babel-polyfill'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { Root } from './views'
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('app-root')
+)
+
